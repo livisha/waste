e3c2ff591100fa383fe09212c7ca7e9fa32df3ca
Livisha jain <61044662+livisha@users.noreply.github.com>
2021-08-21 01:06:22 +0530

    Add files via upload

---
+++ UserDetail.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import './UserDetail.css';
+
+export class UserDetail extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+      };
+    render() {
+        const { values, handleChange } = this.props;
+        return (
+            <MuiThemeProvider>
+            <>
+              <Dialog
+                open
+                fullWidth
+                maxWidth='md'
+              >
+                <AppBar title="Enter User Details" />
+                <TextField
+                  id="text"
+                  placeholder="Enter Your First Name"
+                  label="First Name"
+                  onChange={handleChange('firstName')}
+                  defaultValue={values.firstName}
+                  margin="none"
+                  fullWidth
+                />
+                <br />
+                <TextField
+                  
+                  placeholder="Enter Your Last Name"
+                  label="Last Name"
+                  onChange={handleChange('lastName')}
+                  defaultValue={values.lastName}
+                  margin="none"
+                  fullWidth
+                />
+                <br />
+                <TextField
+                  placeholder="Enter Your Email"
+                  label="Email"
+                  onChange={handleChange('email')}
+                  defaultValue={values.email}
+                  margin="none"
+                  fullWidth
+                />
+                <br />
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={this.continue}
+                >Continue</Button>
+              </Dialog>
+            </>
+          </MuiThemeProvider>
+        )
+    }
+}
+
+export default UserDetail
+
+
